3e1d912de3d17365b822d7c9a7437d547195ba0a
Ishan Goel <igoel.mail@gmail.com>
2020-06-14 09:57:54 +0400

    Update

--- DropLet/src/Receiver.java
+++ DropLet/src/Receiver.java
@@ -1,14 +1,20 @@
+import javax.swing.plaf.TableHeaderUI;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Receiver {
 
 	ServerSocket serverSock = null;
 	Socket senderSock = null;
-	InputStream senderInput = null;
+	InputStream senderReader = null;
+	FileOutputStream fileWriter = null;
+	File file = null;
+	HashMap<String, String> metadata = new HashMap<>();
 
 	public static void main(String[] args) {
 		new Receiver().go();
@@ -20,39 +26,99 @@ public class Receiver {
 			serverSock = new ServerSocket(0);
 			System.out.println("Okie so share this code here: " + serverSock.getLocalPort());
 			System.out.println("The IP is " + (InetAddress.getLocalHost().getHostAddress()));
-			System.out.println("What would like the filename to be?");
-			String path = System.getProperty("user.home") + "/Downloads/" + in.nextLine();
+			System.out.println("What would you like the filename to be? WARNING! IF THIS FILE EXISTS ALREADY, IT WILL BE OVERWRITTEN.");
+			file = new File(System.getProperty("user.home") + "/Downloads/" + in.nextLine());
+			fileWriter = new FileOutputStream(file, true);
 			senderSock = serverSock.accept();
-			senderInput = senderSock.getInputStream();
-			File file = new File(path);
-			int i;
-			String[] temp = path.split("\\.", 2);
-			for (i = 2 ; !file.createNewFile(); i++) {
-				file = new File(temp[0] + " " + i + "." + temp[1]);
-			}
-//			FileWriter filewriter = new FileWriter(path, true);
-//			int characterSent;
-//			while (!senderSock.isClosed() && (characterSent = senderInput.read()) != -1) {
-//				filewriter.write(characterSent);
-//				filewriter.flush();
-//			}
-			FileOutputStream fileStream = new FileOutputStream(file, true);
-			byte[] buffer = new byte[1024];
-			int count;
-			while ((count = senderInput.read(buffer)) > 0) {
-				fileStream.write(buffer, 0, count);
-			}
-
+			senderReader = senderSock.getInputStream();
+			getMetadata();
+			getFile();
 			System.out.println("File saved to Downloads :)");
-			fileStream.flush();
-			fileStream.close();
-			senderInput.close();
-			senderSock.close();
-//			filewriter.close();
+			close();
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("What in the world? I'm a red-shirt so you know what's gonna happen to me");
 			System.exit(42);
-		} ///Users/ishan/Desktop/video.mov
+		}
+	}
+
+	public void getFile () {
+		long fileSizeInBytes = Long.parseLong(metadata.get("FileSizeInBytes"));
+		long numOfBytesReceived = 0L;
+		try {
+			byte[] buffer = new byte[1024]; ///Users/ishan/Desktop/video.mov
+			int count;
+			System.out.println();
+			long beforeTime = System.currentTimeMillis();
+			//int i = 0;
+			while ((count = senderReader.read(buffer)) > 0) {
+				fileWriter.write(buffer, 0, count);
+				numOfBytesReceived += count;
+				//if (i%10 == 0) {
+					progressPercentage(numOfBytesReceived, fileSizeInBytes, (0.001/10.0) * ((double) fileSizeInBytes - (double) numOfBytesReceived) * (((double) System.currentTimeMillis() - (double) beforeTime) / (double) count));
+					beforeTime = System.currentTimeMillis();
+				//}
+				//Thread.sleep(1);
+				//i++;
+			}
+			fileWriter.flush();
+		} catch (Exception e) {e.printStackTrace();}
+	}
+
+	public static void progressPercentage(long done, long total, double estimatedTimeInSeconds) {
+		int size = 20;
+		String iconLeftBoundary = "[";
+		String iconDone = "=";
+		String iconRemain = " ";
+		String iconRightBoundary = "]";
+		if (done > total) {
+			throw new IllegalArgumentException();
+		}
+		int donePercents = (int) ((100 * done) / total);
+		int doneLength = size * donePercents / 100;
+		StringBuilder bar = new StringBuilder(iconLeftBoundary);
+		for (int i = 0; i < size; i++) {
+			if (i < doneLength) {
+				bar.append(iconDone);
+			} else {
+				bar.append(iconRemain);
+			}
+		}
+		bar.append(iconRightBoundary);
+
+		System.out.print("\r" + bar + " " + donePercents + "% (" + done + "/" + total + " B) " + Math.round(10*estimatedTimeInSeconds)/10 + "s left");
+
+		if (done == total) {
+			System.out.print("\n");
+		}
+	}
+
+	public void close (){
+		try {
+			fileWriter.close();
+			senderReader.close();
+			senderSock.close();
+		} catch (Exception e) {e.printStackTrace();}
+	}
+
+	public void getMetadata () {
+		try {
+			byte[] buffer = new byte[1];
+			StringBuilder metadataUnparsed = new StringBuilder();
+			String character = "";
+			while (senderReader.read(buffer) > 0) {
+				if (!(character = new String(buffer, StandardCharsets.UTF_8)).equals("/")) {
+					metadataUnparsed.append(character);
+				} else {
+					break;
+				}
+			}
+			String[] halfParsed = metadataUnparsed.toString().split(",");
+			String[] parsed;
+			for (String s : halfParsed) {
+				parsed = s.split("=");
+				metadata.put(parsed[0], parsed[1]);
+			}
+		} catch (Exception e) {e.printStackTrace();}
 	}
 }
--- DropLet/src/Sender.java
+++ DropLet/src/Sender.java
@@ -1,12 +1,15 @@
 import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Sender {
 
     public Socket senderSock = null;
-    public OutputStream writer = null;
     public Scanner in = new Scanner(System.in);
+    public OutputStream senderWriter = null;
+    public FileInputStream fileReader = null;
+    public File theFileToSend = null;
 
     public static void main(String[] args) {
         new Sender().go();
@@ -15,36 +18,76 @@ public class Sender {
     public void go () {
         try {
             System.out.println("Enter the full path of the file you want to send");
-            File theFileToSend = new File(in.nextLine());
-            //Scanner fileScanner;
+            theFileToSend = new File(in.nextLine());
             System.out.println("Put in the IP please:");
             String ip = in.nextLine();
             System.out.println("Put in the Code please:");
             senderSock = new Socket(ip, in.nextInt());
-            OutputStream sendFile = senderSock.getOutputStream();
-//          fileScanner = new Scanner(TheFileToSend);
-            FileInputStream reader = new FileInputStream(theFileToSend);
-            //BufferedInputStream fileReader = new BufferedInputStream(reader);
+            senderWriter = senderSock.getOutputStream();
+            fileReader = new FileInputStream(theFileToSend);
+            sendMetadata();
+            sendFile();
+            System.out.println("Boom! File sent :)");
+            close();
+        } catch (Exception e) {e.printStackTrace();}
+    }
+
+    public static void progressPercentage(int done, int total, int estimatedTime) {
+        int size = 10;
+        String iconLeftBoundary = "[";
+        String iconDone = "=";
+        String iconRemain = ".";
+        String iconRightBoundary = "]";
+
+        if (done > total) {
+            throw new IllegalArgumentException();
+        }
+        int donePercents = (100 * done) / total;
+        int doneLength = size * donePercents / 100;
+
+        StringBuilder bar = new StringBuilder(iconLeftBoundary);
+        for (int i = 0; i < size; i++) {
+            if (i < doneLength) {
+                bar.append(iconDone);
+            } else {
+                bar.append(iconRemain);
+            }
+        }
+        bar.append(iconRightBoundary);
+
+        System.out.print("\r" + bar + " " + donePercents + "%" + "(" + done + "/" + total + ")" + " " + estimatedTime + "s left");
+
+        if (done == total) {
+            System.out.print("\n");
+        }
+    }
+
+    public void sendMetadata () {
+        try {
+            String metadata = "FileSizeInBytes=" + theFileToSend.length() + "," + "FileName=" + theFileToSend.getName();
+            metadata += "/"; // to signify end of metadata
+            byte[] metadataBytes = metadata.getBytes(StandardCharsets.UTF_8);
+            senderWriter.write(metadataBytes);
+            senderWriter.flush();
+        } catch (Exception e) {e.printStackTrace();}
+    }
+
+    public void sendFile () {
+        try {
             byte[] buffer = new byte[1024];
             int count;
-            while ((count = reader.read(buffer)) > 0) {
-                sendFile.write(buffer, 0, count);
+            while ((count = fileReader.read(buffer)) > 0) {
+                senderWriter.write(buffer, 0, count);
             }
-//            fileScanner.useDelimiter("");
-//            writer.println(TheFileToSend.getName());
-//            writer.flush();
-//            String message;
-//            while (fileScanner.hasNext()) {
-//                message = fileScanner.next();
-//                System.out.println(message);
-//                writer.write(message);
-//                writer.flush();
-//            }
-            reader.close();
-            in.close();
-            sendFile.flush();
-            sendFile.close();
-            System.out.println("Boom! File sent :)");
+            senderWriter.flush();
+        } catch (Exception e) {e.printStackTrace();}
+    }
+
+    public void close () {
+        try {
+            fileReader.close();
+            senderWriter.close();
+            senderSock.close();
         } catch (Exception e) {e.printStackTrace();}
     }
 }
